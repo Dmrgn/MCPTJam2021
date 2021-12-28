be4d279b9686a72f9095b9e561a9c0f93fb92f3c
Daniel Ye <danielyeqt@gmail.com>
2021-12-27 20:38:03 -0500

    add image loading

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-function preload() {
+class Tools {
+    images={}
+    constructor(){
+        loadJSON("data/manifest.json", function(manifest){
+            for(let name in manifest){
+                tool.images[name] = loadImage("data/" + manifest[name])
+            }
+        })
+    }
+}
 
+let tool;
+let img;
+
+function preload() {
+    tool = new Tools();
+    img = function(id) {return tool.images[id]}
 }
 
 function setup() {
-    createCanvas(512, 512, WEBGL);
+    createCanvas(512, 512);
     background(0);
 }
 
 function draw() {
-
+    background(0)
+    fill(255)
+    rect(200, 100, 100, 100)
+    image(img("player"), 0, 0, 100, 100)
 }
